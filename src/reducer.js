218d92bd20e7b760ee0d62e44ebc1b0784519ea4
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-08 09:22:11 +0300

    Completed assignment

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +9,19 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
+function render () {
+  document.body.textContent = state.count;
+}
+
+function dispatch(action){
+  state = changeState (state, action);
+  // return state;
+  render();
+}
+
+// let state = { count: 0 };
 let action = { type: "counter/increment" };
 
 changeState(state, action);
+
+// render();
